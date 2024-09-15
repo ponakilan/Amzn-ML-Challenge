cd9cede1a85be50ae950cbca255a16f7b06cc98d
Akilan K <ponakilan25@gmail.com>
2024-09-15 20:18:48 +0530

    model added

--- src/inference.py
+++ src/inference.py
@@ -1,13 +1,14 @@
 import ollama
 import easyocr
-from lora import LoraModel 
+import pickle
 
 class Inferencer:
-    def __init__(self):
+    def __init__(self, pkl_path):
         self.reader = easyocr.Reader(['en'])
+        self.prompt = pickle.load(open(pkl_path, "rb"))
 
     def get_response(self, image_link, entity_name):
         result = self.reader.readtext(image_link)
         result = " ".join([detection[1] for detection in result])
-        res = ollama.generate(model='llama3.1', prompt=LoraModel.prompt.format(result, entity_name))['response']
+        res = ollama.generate(model='llama3.1', prompt=self.prompt.prompt.format(result, entity_name))['response']
         return result, res
