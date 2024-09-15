77cd9df5feb887c9ec98c62cb7200db859a62c25
Akilan K <ponakilan25@gmail.com>
2024-09-15 20:13:19 +0530

    model added

--- src/inference.py
+++ src/inference.py
@@ -1,13 +1,13 @@
 import ollama
 import easyocr
-from .lora import LoraModel 
+from lora import LoraModel 
 
 class Inferencer:
     def __init__(self):
         self.reader = easyocr.Reader(['en'])
 
-    def get_response(self, image_link, prompt, entity_name):
+    def get_response(self, image_link, entity_name):
         result = self.reader.readtext(image_link)
         result = " ".join([detection[1] for detection in result])
         res = ollama.generate(model='llama3.1', prompt=LoraModel.prompt.format(result, entity_name))['response']
-        return res
+        return result, res
